4631d4f7b4237cf5eab76d61c1b5a5da22abac10
Alexandru Sima <simaalexandru98@gmail.com>
2022-04-10 22:09:36 +0200

    added test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(product_code: 'GR1', name: 'Green Tea', price: 3.11)
+Product.create(product_code: 'SR1', name: 'Strawberries', price: 5.00)
+Product.create(product_code: 'CF1', name: 'Coffee', price: 11.23)
