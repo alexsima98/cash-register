8a454c967a218c98da344dd67c3765d4e7553ad4
Alexandru Sima <simaalexandru98@gmail.com>
2022-04-10 21:52:45 +0200

    passed validation tests for model Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :product_code, presence: true, length: { minimum: 3 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
