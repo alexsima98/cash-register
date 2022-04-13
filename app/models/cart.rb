76a19d61d56d350c10469335ad7bae1a36d4511a
Alexandru Sima <simaalexandru98@gmail.com>
2022-04-14 00:23:51 +0200

    added total_cost method for cart and displayed it in index

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,11 @@
 class Cart < ApplicationRecord
   has_many :cart_products, dependent: :destroy
+
+  def total_cost
+    cost = 0
+    cart_products.each do |product|
+      cost += product.total_cost
+    end
+    cost
+  end
 end
