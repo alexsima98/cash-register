ca21ab32b031094f91dda28ed13f80fd057aae4b
Alexandru Sima <simaalexandru98@gmail.com>
2022-04-14 09:18:29 +0200

    modified remove action of cart_products controller to fit the discount rule

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -6,7 +6,7 @@ class CartProductsController < ApplicationController
 
     if cart_product
       if cart_product.product.discount.type_of_discount === 'pay_one_get_more'
-        cart_product.quantity = cart_product.product.discount.pay_one_get_more + 1
+        cart_product.quantity = cart_product.quantity + (cart_product.product.discount.pay_one_get_more + 1)
         cart.product.save!
       else
         cart_product.quantity += 1
@@ -21,7 +21,12 @@ class CartProductsController < ApplicationController
 
   def remove
     cart_product = @cart.cart_products.find_by(product: @product)
-    cart_product.quantity -= 1
+
+    if cart_product.product.discount.type_of_discount === 'pay_one_get_more'
+      cart_product.quantity = cart_product.quantity - (cart_product.product.discount.pay_one_get_more + 1)
+    else
+      cart_product.quantity -= 1
+    end
 
     if cart_product.quantity.zero?
       cart_product.destroy!
